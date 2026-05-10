7821669bd666a845bcd5a86c542b29ab601ad4a5
Abdul Samad Gilal <abdulsgilal@gmail.com>
2026-05-10 17:26:52 +0500

    Learnt Fast Pass and Slow Pass

---
+++ SlowPass_&_FastPass/findingMiddleElement.py
@@ -0,0 +1,13 @@
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+def middleElement(array):
+    slow = 0
+    fast = 0
+
+    while fast + 1 < len(array):
+        slow += 1
+        fast += 2
+
+    return array[slow]
+
+print(middleElement(array=array))  
