f3c95aebf1a7da9a3d56c545f3fc73ec8aa46d2e
Abdul Samad Gilal <abdulsgilal@gmail.com>
2026-05-12 23:05:58 +0500

    Koko-Eating-Banana-BFSolution

---
+++ Binary Search/Medium Level/koko_eating_banana.py
@@ -0,0 +1,26 @@
+from math import ceil
+
+piles = [3,6,7,11]
+hours = 8
+
+def kokoEatingBanan(piles, hours):
+    maxPile = max(piles)
+    k = [x for x in range (1,maxPile+1)]
+    leftPointer = 0
+    rightPointer = len(k) - 1
+    minimum = maxPile
+
+    while leftPointer <= rightPointer:
+        middleElement = (leftPointer + rightPointer) // 2
+        totalHours = 0
+        for x in piles:
+            totalHours += ceil(x/k[middleElement])
+        if totalHours > hours:
+            leftPointer = middleElement + 1
+        else:
+            minimum = min(minimum, k[middleElement])
+            rightPointer = middleElement - 1
+    return minimum
+
+        
+print(kokoEatingBanan(piles=piles, hours= hours))
