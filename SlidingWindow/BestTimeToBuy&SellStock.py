fa50eac91b25d3312295f649f24e21b9bdf7921f
Abdul Samad Gilal <abdulsgilal@gmail.com>
2026-05-20 03:05:53 +0500

    BestTimeToBuy&SellStock

---
+++ SlidingWindow/BestTimeToBuy&SellStock.py
@@ -0,0 +1,17 @@
+prices = [7,1,5,3,6,4]
+
+def maxProfit(prices):
+    left = 0
+    right = 0
+    maxProfitDay = 0
+
+    while right < len(prices):
+        if prices[right] < prices[left]:
+            left = right
+        else:
+            profit = prices[right] - prices[left]
+            maxProfitDay = max(maxProfitDay, profit)
+        right += 1
+    return maxProfitDay
+
+print(maxProfit(prices=prices))
