2e6db81b806a015710cbec038edbae6875e1830a
Abdul Samad Gilal <abdulsgilal@gmail.com>
2026-05-23 01:26:17 +0500

    Solved LongestRepeatingCharacterReplacement

---
+++ SlidingWindow/longestRepeatingCharacterReplacement.py
@@ -0,0 +1,29 @@
+string = "AABABBA"
+k = 1
+
+def longestRepeatingCharacterReplacement(string, k):
+    hashMap = {}
+    maxFrequency = 0
+    maxLength = 0
+    left = 0
+    right = 0
+
+    for right in range(len(string)):
+        if string[right] not in hashMap:
+            hashMap[string[right]] = hashMap.get(string[right], 0) + 1
+        else:
+            hashMap[string[right]] += 1
+        
+        maxFrequency = max(maxFrequency, hashMap[string[right]])
+
+        windowLength = right - left + 1
+        replacementNeeded = (windowLength - maxFrequency)
+
+        if replacementNeeded > k:
+            hashMap[string[left]] -= 1
+            left += 1
+        
+        maxLength = max(maxLength, right - left + 1)
+    return maxLength
+
+print(longestRepeatingCharacterReplacement(string=string, k=k))
---
+++ playground/frequencyCount.py
@@ -0,0 +1,7 @@
+count = {"a":0,
+         "b": 20}
+
+if "a" in count:
+    count["a"] += 1
+
+print(count)
