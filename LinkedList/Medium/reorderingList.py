479c7d2fcd2ff6e7a081b3930a7cbdfca8005f04
Abdul Samad Gilal <abdulsgilal@gmail.com>
2026-06-07 01:06:11 +0500

    Update reorderingList.py

--- LinkedList/Medium/reorderingList.py
+++ LinkedList/Medium/reorderingList.py
@@ -1,5 +1,47 @@
 from LinkedList.linkedListImplementation import LinkedList
 
-myLinkedList = LinkedList.create([1,2,3,4,5,6,7,8,10])
+head = LinkedList.create([1, 2, 3, 4, 5, 6, 7, 8, 10])
 
-print(myLinkedList)
+# Find middle
+slow = head
+fast = head
+
+while fast and fast.next:
+    slow = slow.next
+    fast = fast.next.next
+
+# Split list
+secondHalf = slow.next
+slow.next = None
+
+
+# Reverse second half
+def reverseLinkedList(head):
+    prev = None
+    current = head
+
+    while current:
+        next_node = current.next
+        current.next = prev
+        prev = current
+        current = next_node
+
+    return prev
+
+secondHalf = reverseLinkedList(secondHalf)
+
+# Merge two halves
+first = head
+second = secondHalf
+
+while second:
+    temp1 = first.next
+    temp2 = second.next
+
+    first.next = second
+    second.next = temp1
+
+    first = temp1
+    second = temp2
+
+print(head)
